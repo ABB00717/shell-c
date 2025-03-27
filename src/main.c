9c1260019bf9e38d74b3465d1728d89c19187355
ABB00717 <eric94528@gmail.com>
2025-03-27 13:57:49 +0800

    Constantly scan user's input

--- src/main.c
+++ src/main.c
@@ -2,17 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+int main(int argc, char* argv[]) {
+    // Flush after every printf
+    setbuf(stdout, NULL);
 
-  printf("$ ");
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  input[strlen(input)-1] = '\0';
+    // Wait for user input
+    char input[100];
+    printf("$ ");
+    while (fgets(input, 100, stdin) != 0) {
+        input[strlen(input) - 1] = '\0';
+        
+        printf("%s: command not found\n", input);
+        printf("$ ");
+    }
 
-  printf("%s: command not found\n", input);
-  return 0;
+    return 0;
 }
