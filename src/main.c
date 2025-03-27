459d48b9b078f9e82174cbd76e0ad2eb20069478
ABB00717 <eric94528@gmail.com>
2025-03-27 13:58:14 +0800

    Implement exit 1 command

--- src/main.c
+++ src/main.c
@@ -6,13 +6,14 @@ int main(int argc, char* argv[]) {
     // Flush after every printf
     setbuf(stdout, NULL);
 
-
     // Wait for user input
     char input[100];
     printf("$ ");
     while (fgets(input, 100, stdin) != 0) {
         input[strlen(input) - 1] = '\0';
-        
+        if (strcmp(input, "exit 0") == 0)
+            break;
+
         printf("%s: command not found\n", input);
         printf("$ ");
     }
